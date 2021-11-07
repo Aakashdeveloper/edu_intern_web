4c05cd3ce735b99fddcd3397437361d246d58a32
aakashdeveloper <ahanda205@gmail.com>
2021-11-07 12:57:42 +0530

    restapi

---
+++ nodejs/restapi/apiDocument.md
@@ -0,0 +1,36 @@
+Page1(Home Page)
+> List Of City (http://localhost:8121/location)
+(this will return all the city) >>>>>>>>>>>>>>>>>>Done
+> Restaurant wrt to city
+(on the basis city return restaurants)
+> List of all meal (http://localhost:8121/mealType)
+(return all the meal types) >>>>>>>>>>>>>>>>>>Done
+
+Page2(Listing Page)
+> Find Restaurant on the basis mealType
+(this will return all the restaurant of particular meal)
+> Filter
+>> Cuisine Filter
+(Search on basis of mealtype and cuisine)
+>> cost Filter
+(Search on basis of mealtype and cost)
+>> sort filter
+(Price high to low and Low to High)
+>> Cuisine + Cost
+((Search on basis of mealtype and cuisine + cost)
+
+Page3(Details Page)
+> Get the details of restaurant on basis of Id
+(http://localhost:8121/restaurant/12)  >>>>>>>>>>>>>>>>>>Done
+> Menu wrt to restaurant
+(return all items of menu for particular restaurant)
+
+Page4(Summary Page)
+> Menu wrt to all ids
+(provides item in basis of id)
+> Post the order
+(Insert order details in db)
+
+Page5(Order Page)
+> List all the order placed >>>>>>>>>>>>>>>>>>Done
+(http://localhost:8121/orders)
--- nodejs/restapi/app.js
+++ nodejs/restapi/app.js
@@ -1,19 +1,75 @@
 var express = require('express');
 var app = express();
 var dotenv = require('dotenv');
+var mongo = require('mongodb');
+var MongoClient = mongo.MongoClient;
 dotenv.config();
+var mongoUrl = process.env.MongoLiveUrl;
 var port = process.env.PORT || 8124;
+// save the database connection
+var db;
 
 // first default route
 app.get('/',(req,res) => {
     res.send("Hiii From Express")
 })
 
+// return all the city
 app.get('/location',(req,res) => {
-    res.send('location url')
+    db.collection('location').find().toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+// return all the mealType
+app.get('/mealType',(req,res) => {
+    db.collection('mealType').find().toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+// return all the restaurants
+app.get('/restaurants',(req,res) => {
+    db.collection('restaurants').find().toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+// restaurant wrt to id
+app.get('/restaurant/:id',(req,res) => {
+    var id = parseInt(req.params.id);
+    db.collection('restaurants').find({"restaurant_id":id}).toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result) 
+    })
+})
+
+// return all the menu
+app.get('/menu',(req,res) => {
+    db.collection('menu').find().toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+// return all the orders
+app.get('/orders',(req,res) => {
+    db.collection('orders').find().toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
 })
 
 
-app.listen(port,()=>{
-    console.log(`listening on port ${port}`)
+// connecting with mongodb
+MongoClient.connect(mongoUrl, (err,client) => {
+    if(err) console.log("Error While Connecting");
+    db = client.db('augintern');
+    app.listen(port,()=>{
+        console.log(`listening on port ${port}`)
+    })
 })
+
